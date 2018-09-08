270c4cd91cdffa0babc7260116da9e38910ed043
Damola Omotosho <damola.omotosho@gmail.com>
2018-09-07 17:32:12 -0700

    Initial commit

---
+++ Hellomicroservices/CurrentDateTimeModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nancy;
+
+namespace Hellomicroservices
+{
+    public class CurrentDateTimeModule : NancyModule
+    {
+        public CurrentDateTimeModule()
+        {
+            Get("/", _ => DateTime.UtcNow);
+        }
+    }
+}
